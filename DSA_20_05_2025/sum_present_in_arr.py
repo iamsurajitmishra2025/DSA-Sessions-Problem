89303e0b97d181c30d3d05500d1f922e7f65f238
Anish Dey <adanishdey59@gmail.com>
2025-06-05 09:57:57 +0530

    Create sum_present_in_arr.py

---
+++ DSA_20_05_2025/sum_present_in_arr.py
@@ -0,0 +1,46 @@
+def sum_present_in_arr(arr: [int], target: int) -> [int]:
+    """
+    Explanation:
+    While iterating the arr, I will keep a track of the current sum of elements and current index.
+    Also, I will keep searching in the hash map, if (sum - target) sum has already occured.
+    For example, target = 33
+    current sum = 38.
+    sum - target = 38 - 33 = 5
+    I want to know, if 5 has already occured. If yes, that means, the sum of subarray in between will be equal to target.
+    :param arr:
+    :param target:
+    :return: [int] of size 2
+    """
+
+
+    n = len(arr)
+    sum_dict = {}
+    sum = 0
+    for i in range(n):
+        sum += arr[i]
+        if sum_dict and (sum - target) in sum_dict:
+            return [sum_dict[(sum - target)] + 1, i]
+        sum_dict[sum] = i
+
+    return [-1, -1]
+
+if __name__ == '__main__':
+    print("Test Case 1: ")
+    arr = [1, 4, 20, 3, 10, 5]
+    target = 33
+    print("Arr", arr)
+    print(sum_present_in_arr(arr, target))  # [2,4]
+
+    print("Test Case 2: ")
+    arr =  [1, 4, 0, 0, 3, 10, 5]
+    target = 7
+    print("Arr", arr)
+    print(sum_present_in_arr(arr, target))  # [1, 4]
+
+    print("Test Case 2: ")
+    arr = [1, 4]
+    target = 0
+    print("Arr", arr)
+    print(sum_present_in_arr(arr, target))  # [-1,-1]
+
+
